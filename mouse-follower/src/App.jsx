97ece1c0f76a0d32d0a1a3cdf40e5d075db64026
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-02-20 10:57:14 +0100

    Update mouse-follower

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,12 +1,57 @@
-import {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 
-function App() {
-  useEffect(() =>{
 
-  })
+const FollowMouse = () => {
+  const [enabled, setEnabled]=useState(false)
+  const [position, setPosition] = useState({x : 0, y : 0})
+
+  useEffect(() =>{
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled){
+      window.addEventListener("pointermove", handleMove)
+    }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove)
+    }
+
+  },[enabled])
   return (
-    <h3>Proyecto</h3>
+    <React.Fragment>
+      <div style={{
+        position: "absolute",
+        background: "rgba(0,0,0,0.5)",
+        border: "1px solid #fff",
+        borderRadius: "50%",
+        opacity: "0.8",
+        pointerEvents: "none",
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={()=> setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </React.Fragment>
+  )
+}
+
+
+
+function App() {
+  
+  return (
+    <main>    
+      <FollowMouse/>
+    </main>
   )
 }
 
